428eacf6fc6894c2fe02ca8d6128d3dc1845f1ab
moustario <muresanantoine@gmail.com>
2022-01-05 19:34:15 +0100

    feat(university): edit uni data

--- erasmusMap.py
+++ erasmusMap.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import json
 app = Flask(__name__)
 
@@ -13,11 +13,15 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/<euc>")
+@app.route("/<euc>", methods=["GET", "POST"])
 def about(euc):
-    # EUC formated to prevents issues with . in url
-    uni = get_from_euc('.'.join(euc.split('-')))
-    return render_template('university.html', uni=uni)
+    if request.method == 'GET':
+        # EUC formated to prevents issues with . in url
+        uni = get_from_euc('.'.join(euc.split('-')))
+        return render_template('university.html', uni=uni)
+    if request.method == 'POST':
+        set_from_euc(request.json['properties']['EUC'], request.json)
+        return {'status': 'operation success'}
 
 
 def get_from_euc(euc):
@@ -27,6 +31,18 @@ def get_from_euc(euc):
         if uni['properties']['EUC'] == euc:
             return uni
     return dict()
+
+
+def set_from_euc(euc, uni):
+    with open('./'+url_for('static', filename='sources/IIA-CZ.PLZEN01-data-valid.geojson'), encoding='utf-8-sig') as json_file:
+        data = json.load(json_file)
+    for university in data['features']:
+        if university['properties']['EUC'] == euc:
+            university['properties'] = uni['properties']
+            university['geometry'] = uni['geometry']
+            print(university)
+    with open('./'+url_for('static', filename='sources/IIA-CZ.PLZEN01-data-valid.geojson'), 'w', encoding='utf-8-sig') as fp:
+        json.dump(data, fp, indent=4)
 
 
 if __name__ == '__main__':
---
+++ static/university.js
@@ -0,0 +1,32 @@
+postUniversity = () => {
+    let payload = {
+        properties: {},
+        geometry: {}
+    };
+    let properties = document.getElementById('uni-properties').children;
+    let geometry = document.getElementById('uni-geometry').children;
+
+    for (index in properties) {
+        let prop = properties[index]
+        if (!isNaN(prop)) break;
+
+        let key = prop.children[0].innerHTML;
+        let value = prop.children[1].value;
+        payload.properties[key] = value;
+    }
+
+    for (index in geometry) {
+        let geo = geometry[index]
+        if (!isNaN(geo)) break;
+
+        let key = geo.children[0].innerHTML;
+        let value = geo.children[1].value;
+        payload.geometry[key] = value;
+    }
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/" + payload.properties.EUC, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify((payload)));
+}
+
+document.getElementById("btn-save").addEventListener("click", postUniversity);
