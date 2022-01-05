c446035e3df38d78c7f62a726f18e6760c602319
moustario <muresanantoine@gmail.com>
2022-01-05 17:42:33 +0100

    feat(university) : added university data page

--- erasmusMap.py
+++ erasmusMap.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 # TODO Sync the 3 sources files : js, csv, geojson
 # TODO Scale on the map
-# TODO clickable points
 # TODO search univerities ?
 # TODO marker cluster ?
 
@@ -14,13 +13,20 @@ def home():
     return render_template('index.html')
 
 
-# @app.route("/<euc>")
-# def about(euc):
-#     print(euc)
-#     with open('./'+url_for('static', filename='sources/IIA-CZ.PLZEN01-data-valid.geojson')) as json_file:
-#         data = json.load(json_file)
-#     print(data)
-#     return render_template('university.html')
+@app.route("/<euc>")
+def about(euc):
+    # EUC formated to prevents issues with . in url
+    uni = get_from_euc('.'.join(euc.split('-')))
+    return render_template('university.html', uni=uni)
+
+
+def get_from_euc(euc):
+    with open('./'+url_for('static', filename='sources/IIA-CZ.PLZEN01-data-valid.geojson'), encoding='utf-8-sig') as json_file:
+        data = json.load(json_file)
+    for uni in data['features']:
+        if uni['properties']['EUC'] == euc:
+            return uni
+    return dict()
 
 
 if __name__ == '__main__':
